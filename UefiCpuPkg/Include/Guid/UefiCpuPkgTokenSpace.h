a1cca633ca69584f82eca62ba768043c93594e64
Michael Kinney <michael.d.kinney@intel.com>
2015-10-04 15:48:19 -0700

    UefiCpuPkg: Add missing UefiCpuPkgTokenSpace GUID include file
    Add missing include files for the token space GUID for the UefiCpuPkg
    Add path to the token space include file to the UefiCpuPkg DEC file.
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Michael Kinney <michael.d.kinney@intel.com>

---
+++ UefiCpuPkg/Include/Guid/UefiCpuPkgTokenSpace.h
@@ -0,0 +1,25 @@
+/** @file
+GUID for UefiCpuPkg PCD Token Space
+
+Copyright (c) 2015, Intel Corporation. All rights reserved.<BR>
+This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef _UEFICPUPKG_TOKEN_SPACE_GUID_H_
+#define _UEFICPUPKG_TOKEN_SPACE_GUID_H_
+
+#define UEFICPUPKG_TOKEN_SPACE_GUID \
+  { \
+    0xac05bf33, 0x995a, 0x4ed4, { 0xaa, 0xb8, 0xef, 0x7a, 0xe8, 0xf, 0x5c, 0xb0 } \
+  }
+
+extern EFI_GUID gUefiCpuPkgTokenSpaceGuid;
+
+#endif
