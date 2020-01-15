2cc1b2ae24b8ae8c0bef3bc4e04e371e3885ca85
Michael D Kinney <michael.d.kinney@intel.com>
2020-01-15 13:12:50 -0800

    BaseTools: Update copyright year
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>

--- BaseTools/Source/Python/Workspace/DscBuildData.py
+++ BaseTools/Source/Python/Workspace/DscBuildData.py
@@ -1,7 +1,7 @@
 ## @file
 # This file is used to create a database used by build tool
 #
-# Copyright (c) 2008 - 2019, Intel Corporation. All rights reserved.<BR>
+# Copyright (c) 2008 - 2020, Intel Corporation. All rights reserved.<BR>
 # (C) Copyright 2016 Hewlett Packard Enterprise Development LP<BR>
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 #
